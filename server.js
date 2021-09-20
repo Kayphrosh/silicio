ddbf2cbcbbca2318967378175e3ab5a92251dd3f
kunbi <olakunbiolabode@yahoo.com>
2021-09-20 21:49:37 +0100

    Correcting the routes in server

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path  = require('path')
 const bodyParser = require('body-parser')
-const { urlencoded } = require('body-parser')
+const { urlencoded, json } = require('body-parser')
 const WaitingRoute  = require('./Routes/WaitingRoute')
 const mongoose  = require("mongoose")
 const dotenv = require('dotenv')
@@ -17,18 +17,19 @@ db.once('open', () => {
     console.log('Database Connection Established')
 })
 
-const port = "8000" || process.env.PORT
+const port = "7000" || process.env.PORT
 const app = express()
 app.use('/public',express.static(path.join(__dirname, 'static')))
 app.set('view engine', 'ejs')
 app.use(urlencoded({ extended: false }))
 app.use(bodyParser.json())
+app.use(json())
 
 app.get("/", (req, res) => {
     res.render('index');
 });
 
-// app.use('/', WaitingRoute)
-app.listen('8000', (err) => {
+app.use('/', WaitingRoute)
+app.listen('7000', (err) => {
     console.log('Server started on port', port)
 })
